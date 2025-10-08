da5d8c4c3ea705387966d88dd79c1e0a31233cae
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-08 15:02:48 +0300

    LT-2-4 create smth

---
+++ utils/src/main/kotlin/lesson_2/lesson_2_4.kt
@@ -0,0 +1,10 @@
+package org.example.utils.lesson_2
+
+fun main(){
+    val crystal = 7
+    val metal = 11
+    val buffCrystal = crystal * 20 / 100
+    val buffMetal = metal * 20 / 100
+    println("Кристалическая руда: $buffCrystal \nЖелезная руда: $buffMetal ")
+
+}
