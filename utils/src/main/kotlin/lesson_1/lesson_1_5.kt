e994b3c74787003207d239a94b75dfb8b6002153
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-08 16:43:22 +0300

    LT-1-5 create smth

--- utils/src/main/kotlin/lesson_1/lesson_1_5.kt
+++ utils/src/main/kotlin/lesson_1/lesson_1_5.kt
@@ -1,13 +1,15 @@
 package org.example.utils.lesson_1
 
+const val z = 60
+
 fun main(){
     val totalSeconds = 6480
-    var hours = totalSeconds / 3600
+    var hours = totalSeconds / (z*z)
     val formattedHours = String.format("%02d",hours)
-    val remainingSecondsAfterHours = totalSeconds % 3600
-    val minutes = remainingSecondsAfterHours / 60
-    var seconds = remainingSecondsAfterHours % 60
+    val remainingSecondsAfterHours = totalSeconds % (z*z)
+    val minutes = remainingSecondsAfterHours / z
+    var seconds = remainingSecondsAfterHours % z
     val formattedSeconds = String.format("%02d",seconds)
-    println("${formattedHours}:$minutes:${formattedSeconds}")
+    println("$formattedHours:$minutes:$formattedSeconds")
 
 }
