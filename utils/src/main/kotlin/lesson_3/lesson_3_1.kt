ea2ae152aaf7d3bbeb75cdaff34d79fd5ec87c92
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-09 12:17:20 +0300

    LT-3-1 create smth

---
+++ utils/src/main/kotlin/lesson_3/lesson_3_1.kt
@@ -0,0 +1,11 @@
+package org.example.utils.lesson_3
+
+fun  main(){
+    val name = "Aik"
+    val day = "Добрый день!"
+    val night = "Добрый вечер!"
+    val x = ( day + name)
+    val z = (night + name)
+    println(x)
+    println(z)
+}
