08c73addca5c22310ca2694c08a11de676e315cf
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-09 17:13:19 +0300

    LT-2-5 corrected smth

--- utils/src/main/kotlin/lesson_2/lesson_2_5.kt
+++ utils/src/main/kotlin/lesson_2/lesson_2_5.kt
@@ -2,15 +2,17 @@ package org.example.utils.lesson_2
 
 import kotlin.math.pow
 
-const val P = 70_000
-
-fun main(){
-    val r = 0.167
-    val n = 1
-    val t = 20
-    val a = (1 + r/n)
-    val z = a.pow(t)
-    val x = P * z
-    println( String.format("%.3f",x))
+fun main() {
+    val capital = 70_000
+    val percent = 16.7
+    val maksPercent = 100
+    val accrual = 1
+    val years = 20
+    val formulaVariable = 1
+    val recalculationPercent = percent / maksPercent
+    val calculation = (formulaVariable + recalculationPercent / accrual)
+    val recalculation = calculation.pow(years)
+    val totalAmount = capital * recalculation
+    println(String.format("%.3f", totalAmount))
 
 }
