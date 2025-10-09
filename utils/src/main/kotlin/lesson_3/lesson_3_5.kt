7b9e7eeec5ea0dd64bd1fa49e1072c84061b3d19
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-09 15:21:54 +0300

    LT-3-5 create smth

---
+++ utils/src/main/kotlin/lesson_3/lesson_3_5.kt
@@ -0,0 +1,12 @@
+package org.example.utils.lesson_3
+
+
+fun main(){
+    var move: String = ("D2-D4;0")
+    val data = move.split("-",";")
+    val where = data[0]
+    val step = data[1]
+    val number = data[2]
+    val message = ("$where \n$step \n$number")
+    println(message)
+}
