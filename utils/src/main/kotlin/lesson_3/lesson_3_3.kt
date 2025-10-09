b6608fbc3702cb7a9064c91e4fca67838ee40acb
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-09 18:11:20 +0300

    LT-3-3 corrected smth

--- utils/src/main/kotlin/lesson_3/lesson_3_3.kt
+++ utils/src/main/kotlin/lesson_3/lesson_3_3.kt
@@ -1,9 +1,9 @@
 package org.example.utils.lesson_3
 
-fun main(){
-    val x = 5
-    for (i in 1..9){
-        val result = x * i
-        println("$x * $i = $result\t")
+fun main() {
+    val number = 5
+    for (i in 1..9) {
+        val result = number * i
+        println("$number * $i = $result\t")
     }
 }
