628ba60787ad8b9be18ab7f57df1e5e3461d01b5
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-09 12:37:01 +0300

    LT-3-2 create smth

---
+++ utils/src/main/kotlin/lesson_3/lesson_3_2.kt
@@ -0,0 +1,12 @@
+package org.example.utils.lesson_3
+
+fun main(){
+    val name = "Татьяна"
+    var surname = "Андреева"
+    var age = 20
+    val patronymic = "Сергеевна"
+    println("$surname $name $patronymic, $age")
+    surname = "Сидорова"
+    age = 22
+    println("$surname $name $patronymic, $age")
+}
