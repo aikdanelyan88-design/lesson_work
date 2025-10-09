e1a5d435150e5deaa0e977e0537c974acdd339a1
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-09 16:49:15 +0300

    LT-2-4 corrected smth

--- utils/src/main/kotlin/lesson_2/lesson_2_4.kt
+++ utils/src/main/kotlin/lesson_2/lesson_2_4.kt
@@ -1,10 +1,12 @@
 package org.example.utils.lesson_2
 
-fun main(){
+fun main() {
     val crystal = 7
     val metal = 11
-    val buffCrystal = crystal * 20 / 100
-    val buffMetal = metal * 20 / 100
+    val pecentageBuff = 20
+    val possibleBuff = 100
+    val buffCrystal = crystal * pecentageBuff / possibleBuff
+    val buffMetal = metal * pecentageBuff / possibleBuff
     println("Кристалическая руда: $buffCrystal \nЖелезная руда: $buffMetal ")
 
 }
