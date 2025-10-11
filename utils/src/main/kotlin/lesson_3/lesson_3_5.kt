1d25f3590c82d02b02c384ebfcef7afdf569975c
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-11 15:53:11 +0300

    KS-3-5 corrected comments

--- utils/src/main/kotlin/lesson_3/lesson_3_5.kt
+++ utils/src/main/kotlin/lesson_3/lesson_3_5.kt
@@ -1,9 +1,8 @@
 package org.example.utils.lesson_3
 
-
-fun main(){
-    var move: String = ("D2-D4;0")
-    val data = move.split("-",";")
+fun main() {
+    val move: String = ("D2-D4;0")
+    val data = move.split("-", ";")
     val where = data[0]
     val step = data[1]
     val number = data[2]
