c6d15717b73ccdf3e63edf66642c83584740c424
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-11 15:57:53 +0300

    KS-4-3 corrected comments

--- utils/src/main/kotlin/lesson_4/lesson_4_3.kt
+++ utils/src/main/kotlin/lesson_4/lesson_4_3.kt
@@ -5,6 +5,6 @@ fun main() {
     val awningOpen: Boolean = true
     val airHumidity: Int = 20
     val timeOfYear: String = "winter"
-    val favorableConditions = sunnyWeather && awningOpen && airHumidity == 20 && timeOfYear != "winter"
+    val favorableConditions = (sunnyWeather && awningOpen && airHumidity == 20 && timeOfYear != "winter")
     println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 }
