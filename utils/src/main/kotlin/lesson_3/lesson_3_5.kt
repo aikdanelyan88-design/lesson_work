5250117d292dc276becf8c888697bf9aff67751b
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-12 00:26:17 +0300

    KS-3-5 corected  the task

--- utils/src/main/kotlin/lesson_3/lesson_3_5.kt
+++ utils/src/main/kotlin/lesson_3/lesson_3_5.kt
@@ -1,11 +1,11 @@
 package org.example.utils.lesson_3
 
 fun main() {
-    val move: String = ("D2-D4;0")
+    val move: String = "D2-D4;0"
     val data = move.split("-", ";")
     val where = data[0]
     val step = data[1]
     val number = data[2]
-    val message = ("$where \n$step \n$number")
-    println(message)
+
+    println("$where \n$step \n$number")
 }
