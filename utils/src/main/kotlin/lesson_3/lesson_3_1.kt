a38fab0d08551046ce6f8955eba786b74d4435ac
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-11 13:56:30 +0300

    KS-3-1 corrected comments

--- utils/src/main/kotlin/lesson_3/lesson_3_1.kt
+++ utils/src/main/kotlin/lesson_3/lesson_3_1.kt
@@ -2,10 +2,12 @@ package org.example.utils.lesson_3
 
 fun main() {
     val name = "Aik"
-    val day = "Добрый день!"
-    val night = "Добрый вечер!"
-    var greetingsDay = day + name
-    val greetingsNight = (night + name)
+    val day = "Добрый день"
+    val night = "Добрый вечер"
+    val sign = ':'
+    var greetingsDay = (day + sign + name)
+    val greetingsNight = (night + sign + name)
+
     println(greetingsDay)
     println(greetingsNight)
 }
