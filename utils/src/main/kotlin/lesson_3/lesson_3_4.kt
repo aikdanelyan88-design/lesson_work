9d470e47e3a76e1f39fe820777d65c54579bb705
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-11 15:50:55 +0300

    KS-3-4 corrected comments

--- utils/src/main/kotlin/lesson_3/lesson_3_4.kt
+++ utils/src/main/kotlin/lesson_3/lesson_3_4.kt
@@ -1,13 +1,12 @@
 package org.example.utils.lesson_3
 
-fun main(){
+fun main() {
     var where = "Е2"
-    var step  = "Е4"
+    var step = "Е4"
     var numberMove = "1"
-    var move = ("[$where-$step;$numberMove]")
-    println(move)
+    println("[$where-$step;$numberMove]")
+
     where = "D2"
     step = "D3"
-    move =("[$where-$step;$numberMove]")
-    println(move)
+    println("[$where-$step;$numberMove]")
 }
