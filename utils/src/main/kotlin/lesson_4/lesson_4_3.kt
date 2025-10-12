c5df886ecf9cfbcba80976471990650da060ee7f
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-12 17:24:04 +0300

    KS-4-3 corected  the task

--- utils/src/main/kotlin/lesson_4/lesson_4_3.kt
+++ utils/src/main/kotlin/lesson_4/lesson_4_3.kt
@@ -1,10 +1,19 @@
 package org.example.utils.lesson_4
 
+const val WEATHER_SUNNY: Boolean = true
+const val AWNING_POSITION: Boolean = true
+const val HUMIDITY: Int = 20
+const val TIME_OF_YEAR: String = "winter"
+
 fun main() {
-    val sunnyWeather: Boolean = true
-    val awningOpen: Boolean = true
+    val isSunny: Boolean = true
+    val isAwningOpen: Boolean = true
     val airHumidity: Int = 20
     val timeOfYear: String = "winter"
-    val favorableConditions = (sunnyWeather && awningOpen && airHumidity == 20 && timeOfYear != "winter")
+    val favorableConditions = (isSunny == WEATHER_SUNNY) &&
+            (isAwningOpen == AWNING_POSITION) &&
+            (airHumidity == HUMIDITY) &&
+            (timeOfYear !== TIME_OF_YEAR)
+
     println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 }
